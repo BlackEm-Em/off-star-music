5437d0cf4174749d5733f88ea85808c27cd82e42
79252 <79252@BLACKEM>
2023-11-13 07:36:25 +0300

    Добавьте файлы проекта.

---
+++ starm/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Xamarin.Forms;
+
+namespace starm
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            // MainPage теперь обернута в NavigationPage
+            MainPage = new NavigationPage(new MainPage());
+        }
+
+        protected override void OnStart()
+        {
+            // Обработка начала работы приложения
+        }
+
+        protected override void OnSleep()
+        {
+            // Обработка перехода приложения в режим сна
+        }
+
+        protected override void OnResume()
+        {
+            // Обработка возобновления работы приложения
+        }
+    }
+}
---
+++ starm/Views/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using starm.Views;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace starm
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            NavigationPage.SetHasNavigationBar(this, false); // Скрыть панель навигации
+        }
+
+        private async void NavigateToPage1_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new RegistrationPage ());
+        }
+
+
+    }
+}
